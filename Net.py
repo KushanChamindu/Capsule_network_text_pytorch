f3467acaa20edc678d644aaa59d5ef483f0c7cc6
Kushan <ra.kushan1997@gmail.com>
2021-10-04 18:28:21 +0530

    elu layer and conv_layer updated almost finished

--- Net.py
+++ Net.py
@@ -9,7 +9,7 @@ from torch.nn.parameter import Parameter
 
 
 class Elu_layer(nn.Module):
-    def __init__(self, num_features, in_channels, out_channels, filter_ensemble_size, num_filters):
+    def __init__(self, num_features, in_channels, out_channels, filter_ensemble_size):
         super(Elu_layer, self).__init__()
         self.elu_layer_conv = nn.Conv2d(in_channels=in_channels,
                                out_channels=out_channels,
@@ -18,37 +18,27 @@ class Elu_layer(nn.Module):
                                    filter_ensemble_size, num_features),
                                stride=1, bias=False,
                                )
-        # self.elu_layer_conv = nn.ModuleList([
-        #     nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(
-        #                             filter_ensemble_size, num_features), stride=1,bias=False, padding=0)
-        #     for _ in range(num_capsules)])
         self.batch_normalization_layer = nn.BatchNorm2d(
-        num_filters, eps=1e-05, momentum=0.1, affine=True)
+        out_channels, eps=1e-05, momentum=0.1, affine=True)
 
         self.elu_layer = nn.ELU(alpha=1.0, inplace=False)
 
     def forward(self, x):
-        # elu_layer_conv = [capsule(x) for capsule in self.elu_layer_conv]
-        # u = torch.stack(u, dim=1)
-        # elu_layer_conv = u.view(x.size(0), self.num_routes, -1)
-        print(x.type())
         elu_layer_conv = self.elu_layer_conv(x)
-        print(elu_layer_conv.size)
-        print(elu_layer_conv)
         batch_normalized = self.batch_normalization_layer(elu_layer_conv)
-        print(batch_normalized)
         return self.elu_layer(batch_normalized)  # F.elu(batch_normalized, alpha=1.0, inplace=False)  
 class Multiply(nn.Module):
   def __init__(self):
     super(Multiply, self).__init__()
 
   def forward(self, tensors):
+    # result = torch.mul(tensors[0],tensors[1])
     result = torch.ones(tensors[0].size())
     for t in tensors:
       result *= t
-    return t
+    return result
 class ConvLayer(nn.Module):
-    def __init__(self, num_features, in_channels, out_channels, filter_ensemble_size):
+    def __init__(self, num_features, in_channels, out_channels, filter_ensemble_size,dropout_ratio):
         super(ConvLayer, self).__init__()
         self.conv_layer = nn.Conv2d(in_channels=in_channels,
                                 out_channels=out_channels,
@@ -57,23 +47,18 @@ class ConvLayer(nn.Module):
                                     filter_ensemble_size, num_features),
                                 stride=1, bias=False,
                                 )
-        # self.conv_layer = nn.ModuleList([
-        #     nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(
-        #                             filter_ensemble_size, num_features), stride=1,bias=False, padding=0)
-        #     for _ in range(num_capsules)])
-        self.batch_normalization_layer = nn.BatchNorm1d(
-        num_features, eps=1e-05, momentum=0.1, affine=True)
-        # self.multiply_layer = Multiply()
-        # self.dropout = nn.Dropout(p=0.5, inplace=False)
+        self.batch_normalization_layer = nn.BatchNorm2d(
+        out_channels, eps=1e-05, momentum=0.1, affine=True)
+        self.multiply_layer = Multiply()
+        self.dropout = nn.Dropout(p=dropout_ratio, inplace=False)
         
 
-    def forward(self, x):
+    def forward(self, elu_layer,x):
         conv_layer = self.conv_layer(x)
-        # conv_layer = [capsule(x) for capsule in self.conv_layer]
-        # u = torch.stack(u, dim=1)
-        # conv_layer = u.view(x.size(0), self.num_routes, -1)
         batch_normalized = self.batch_normalization_layer(conv_layer)
-        return batch_normalized
+        # gate = torch.mul(elu_layer,batch_normalized)
+        gate_layer = self.multiply_layer([elu_layer,batch_normalized])
+        return self.dropout(gate_layer)
 class Reshape(nn.Module):
     def __init__(self, *args):
         super(Reshape, self).__init__()
@@ -104,33 +89,46 @@ class ConvCapsLayer(nn.Module):
         return self.dropout_capsule(relu_output)
 
 class ExtractionNet(nn.Module):
-    def __init__(self, word_embed_dim, output_size, hidden_size, capsule_num):
+    def __init__(self, word_embed_dim, output_size, hidden_size, capsule_num,filter_ensemble_size,dropout_ratio):
         super(ExtractionNet, self).__init__()
 
-        self.elu_layer = Elu_layer(in_channels=1, out_channels= 4, num_features=10, filter_ensemble_size= 3,num_filters=4)
-        # self.conv_layer = ConvLayer(in_channels=1, out_channels=1,num_features=1,)
+        self.elu_layer = Elu_layer(in_channels=1, out_channels= capsule_num, num_features=word_embed_dim, filter_ensemble_size= filter_ensemble_size)
+        self.conv_layer = ConvLayer(in_channels=1, out_channels=capsule_num,num_features=word_embed_dim,filter_ensemble_size= filter_ensemble_size,dropout_ratio=dropout_ratio)
 
     def forward(self,x):
         elu_layer = self.elu_layer(x)
-        print(elu_layer)
-model = ExtractionNet(300,30,128,16)
+        conv_layer = self.conv_layer(elu_layer,x)
+        return(conv_layer)
+
 # x= torch.tensor([1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10], dtype=torch.long)
 # x = x.view(1,1,3,10)
 # x = x.type(torch.long)
-x = torch.tensor([[[[ 1,  2,  3,  4,  5,  6,  7,  8, 10, 10],
-          [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
-          [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10]]],
-          [[[ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
-          [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
-          [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10]]]
-          ], dtype=torch.float32)
+
+
+# model = ExtractionNet(word_embed_dim=10,output_size=100,hidden_size=128,capsule_num=4,filter_ensemble_size=3, dropout_ratio=0.8)
+# x = torch.tensor([[[[ 1,  2,  3,  4,  5,  6,  7,  8, 10, 10],
+#           [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
+#           [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10]]],
+#           [[[ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
+#           [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
+#           [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10]]]
+        #   ], dtype=torch.float32)
+
+model = ExtractionNet(word_embed_dim=300,output_size=100,hidden_size=128,capsule_num=16,filter_ensemble_size=3,dropout_ratio=0.8)
+x = torch.rand(10,1,30,300)
 print(x.size())
-# x = torch.rand(1,1,3,10)
-model(x)
-for i in model.parameters():
-    print(i.size())
+output=model(x)
+print(output.size())
+# print(output)
+# for i in model.parameters():
+#     print(i.size())
 # for i in list(model.parameters()): print(i.shape)
-print(torch.cuda.is_available())
+# print(torch.cuda.is_available())
+
+# x1 = torch.tensor([1,2,3,4]).view(2,2)
+# x2 = torch.tensor([1,2,3,4]).view(2,2)
+
+# print(Multiply()([x1,x2]))
         
     
         
