2df25e2cf9a18595e6faaf0175064c568825c030
Kushan <ra.kushan1997@gmail.com>
2021-10-05 14:44:05 +0530

    remove comments

--- Net.py
+++ Net.py
@@ -38,19 +38,6 @@ class ExtractionNet(nn.Module):
         capsule_norm = self.capsule_norm(routing_2)
         return(capsule_norm)
 
-# x= torch.tensor([1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10], dtype=torch.long)
-# x = x.view(1,1,3,10)
-# x = x.type(torch.long)
-
-
-# model = ExtractionNet(word_embed_dim=10,output_size=100,hidden_size=128,capsule_num=4,filter_ensemble_size=3, dropout_ratio=0.8, intermediate_size=(16, 4), sentence_length=3)
-# x = torch.tensor([[[[ 1,  2,  3,  4,  5,  6,  7,  8, 10, 10],
-#           [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
-#           [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10]]],
-#           [[[ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
-#           [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
-#           [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10]]]
-#           ], dtype=torch.float32)
 
 model = ExtractionNet(word_embed_dim=300, output_size=4, hidden_size=128,
                       capsule_num=16, filter_ensemble_size=3, dropout_ratio=0.8, intermediate_size=(128, 8), sentence_length=30)
@@ -60,19 +47,4 @@ print(x.size())
 output = model(x)
 print(output.size())
 print(model)
-# print(output)
-# for i in model.parameters():
-#     print(i.size())
-# for i in list(model.parameters()): print(i.shape)
-# print(torch.cuda.is_available())
-# x1 = torch.tensor([1, 2, 3, 4])
-# y = x1.reshape(2, 2)
-# print(y.size())
-# print(y)
-# z = (torch.unsqueeze(y, 1))
-# print(z.size())
-# print(z)
-# x1 = torch.tensor([1,2,3,4]).view(2,2)
-# x2 = torch.tensor([1,2,3,4]).view(2,2)
 
-# print(Multiply()([x1,x2]))
